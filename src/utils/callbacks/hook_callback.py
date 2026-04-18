d912739943dc280e497f9fd3cb5142770c99a342
GrilledPotato-161103 <25025069@vnu.edu.vn>
2026-04-18 15:23:43 +0700

    ,.

--- src/utils/callbacks/hook_callback.py
+++ src/utils/callbacks/hook_callback.py
@@ -65,7 +65,8 @@ class AdversarialVizCallback(pl.Callback):
         # B, N
         jumps = torch.stack([jumps] * losses.shape[0], axis=0).to(losses.device)
         # B, N, 2
-        jumps_one = torch.nn.functional.pad(jumps.unsqueeze(1), (0, 1), value=1)
+        jumps_one = torch.nn.functional.pad(jumps, (0, 1), value=1)
+        print(jumps_one.shape)
         weights, _, _, _ = torch.linalg.lstsq(jumps_one.flatten(0), losses_gain.flatten(0))
 
         # Get logarithm weight as correlation
